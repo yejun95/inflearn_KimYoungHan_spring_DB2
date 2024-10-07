8ca1f9b718aba0d972c7eaf658396aac9220e8f0
xodlf100 <xodlf100@gmail.com>
2024-10-08 00:15:35 +0900

    add JdbcV2Config

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,7 +10,8 @@ import org.springframework.context.annotation.Profile;
 
 
 //@Import(MemoryConfig.class)
-@Import(JdbcdTemplateV1Config.class)
+//@Import(JdbcdTemplateV1Config.class)
+@Import(JdbcdTemplateV2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
