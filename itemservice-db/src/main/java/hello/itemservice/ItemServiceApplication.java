50c57c545c81cac663fae8b41fe58624156270b0
xodlf100 <xodlf100@gmail.com>
2024-10-08 00:23:09 +0900

    feat add SimpleJdbcInsert

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Profile;
 
 //@Import(MemoryConfig.class)
 //@Import(JdbcdTemplateV1Config.class)
-@Import(JdbcdTemplateV2Config.class)
+//@Import(JdbcdTemplateV2Config.class)
+@Import(JdbcdTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
