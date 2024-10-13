7d0cc152b10089e0a65c23d8af14b52d0d8e5d24
xodlf100 <xodlf100@gmail.com>
2024-10-13 17:42:41 +0900

    feat Transactional AOP 적용되지 않는 예시 test

---
+++ springtx/src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,67 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    static class CallService {
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            //여기서 트랜잭션 AOP Proxy를 거치지 않고 실제 객체로 호출되버림 -> 트랜잭션 적용이 안됨
+            //메서드 앞에 별도 참조가 없으면 this.internal 이란 뜻이기 때문에 자기 자신의 인스턴스 주소를 호출함
+            internal();
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", readOnly);
+        }
+    }
+}
