86d1596142094ba4e498ed5da8088bf7045a36f2
xodlf100 <xodlf100@gmail.com>
2024-10-17 15:09:24 +0900

    test Trasaction 1 fail -> all rollback

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -94,6 +95,23 @@ class MemberServiceTest {
 
         assertThatThrownBy(() -> memberService.joinV1(username))
                 .isInstanceOf(RuntimeException.class);
+
+        //논리 트랜잭션인 log에서 Exception이 발생했기 때문에 모든 데이터가 롤백됨
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService @Transactional: On
+     * memberRepository @Transactional: ON
+     * logRepository @Transactional: ON Exception
+     */
+    @Test
+    void recoverException_fail() {
+        String username = "로그예외_recoverException_fail";
+
+        assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class);
 
         //논리 트랜잭션인 log에서 Exception이 발생했기 때문에 모든 데이터가 롤백됨
         assertTrue(memberRepository.find(username).isEmpty());
